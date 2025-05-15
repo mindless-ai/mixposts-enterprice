476b6966a28324187928f8d3eeee82b4b96e8ed3
Mariano Rivera <rivveramariano@gmail.com>
2025-05-15 00:15:33 -0600

    dsffsdf

--- routes/templates.php
+++ routes/templates.php
@@ -3,7 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/workspace/{workspace}/templates', function ($workspace) {
-    return inertia('Workspace/Templates/Index', [
-        'workspace' => $workspace
-    ]);
+    return redirect()->away('https://www.google.com');
 })->name('mixpost.templates.index');
