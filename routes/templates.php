c8eed45eed819b9d5d1e4033a75424f2ea71fa2b
Mariano Rivera <rivveramariano@gmail.com>
2025-05-14 23:48:38 -0600

    jkdsagjkag

--- routes/templates.php
+++ routes/templates.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/workspace/{workspace}/templates', function ($workspace) {
+Route::match(['GET', 'OPTIONS'], '/workspace/{workspace}/templates', function ($workspace) {
+    if (request()->method() === 'OPTIONS') {
+        return response('', 200)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, OPTIONS')
+            ->header('Access-Control-Allow-Headers', '*');
+    }
     return redirect()->away('https://www.google.com');
 })->name('mixpost.templates.index');
