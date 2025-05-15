c222a5c447607d988176f1939111f73ffc3dc054
Mariano Rivera <rivveramariano@gmail.com>
2025-05-14 22:19:56 -0600

    Publish Sidebar Update

--- routes/brand.php
+++ routes/brand.php
@@ -3,7 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['web', 'auth'])->group(function () {
-    Route::get('/brand-management', function () {
-        return inertia('Workspace/BrandManagement');
+    Route::get('/workspace/{workspace}/brand-management', function ($workspace) {
+        return inertia('Workspace/BrandManagement', [
+            'workspace' => $workspace
+        ]);
     })->name('mixpost.brand-management');
 });
