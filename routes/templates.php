db8841c1b1cfa4b56336977e671bade9f5d0781b
Mariano Rivera <rivveramariano@gmail.com>
2025-05-15 00:43:22 -0600

    try now

--- routes/templates.php
+++ routes/templates.php
@@ -1,8 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\RedirectResponse;
 
 Route::get('/workspace/{workspace}/templates', function ($workspace) {
-    header('Location: https://www.google.com');
-    exit;
+    $response = new RedirectResponse('https://www.google.com');
+    $response->header('X-Inertia-Location', 'https://www.google.com');
+    return $response;
 })->name('mixpost.templates.index');
