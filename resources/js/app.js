5389adc25e65189c25414b71db1a5946015db0a6
Mariano Rivera <rivveramariano@gmail.com>
2025-05-19 10:07:01 -0600

    ASdasd

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,27 +1,4 @@
 import './bootstrap';
-import { createApp, h } from 'vue';
-import { createInertiaApp } from '@inertiajs/vue3';
-
-// Import custom components and overrides
-import { registerCustomComponents } from './register-custom-components';
-import './translation-override';
 
 // Import custom dashboard enhancements
 import './dashboard-customizer';
-
-// Register custom components when the app is created
-createInertiaApp({
-    resolve: name => {
-        const pages = import.meta.glob('./Pages/**/*.vue', { eager: true });
-        return pages[`./Pages/${name}.vue`];
-    },
-    setup({ el, App, props, plugin }) {
-        const app = createApp({ render: () => h(App, props) })
-            .use(plugin);
-            
-        // Register our custom components
-        registerCustomComponents(app);
-        
-        app.mount(el);
-    },
-});
