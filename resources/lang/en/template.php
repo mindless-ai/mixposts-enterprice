3121d9a11d9ea1a1f2ead8b2c8a71a457208032a
Mariano Rivera <rivveramariano@gmail.com>
2025-05-20 08:40:55 -0600

    Checking

---
+++ resources/lang/en/template.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'templates' => 'Mariano',
+    // We only need to override the specific keys we want to change
+    // Other keys will fall back to the vendor's translation file
+];
